43f01f7f00f648a14d3224da2a990203345bcf0a
rauf22 <rafik2516@gmail.com>
2018-01-10 16:25:03 +0300

    change all in postgres

--- Gemfile
+++ Gemfile
@@ -29,7 +29,9 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-
+gem 'jquery-unslider-rails', '~> 2.0', '>= 2.0.1'
+gem 'devise'
+gem 'rails_admin'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+	  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :users
+  get 'pages/home'
+
+  get 'pages/men'
+
+  get 'pages/women'
+
+  get 'pages/kids'
+
+  get 'pages/sale'
+
+  get 'pages/aboutAs'
+
+  get 'pages/support'
+
+  root 'pages#home'
+
+	resources :menu
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
