9c16e502135ab6778de27e0a074cc212a1166c52
rauf22 <rafik2516@gmail.com>
2018-01-10 18:35:35 +0300

    men

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20180110112409) do
     t.decimal "price", precision: 15, scale: 2, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "order_id", null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -34,8 +36,6 @@ ActiveRecord::Schema.define(version: 20180110112409) do
     t.decimal "sub_total", precision: 15, scale: 2, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "order_id", null: false
-    t.index ["order_id"], name: "index_orders_on_order_id"
   end
 
   create_table "product_categories", force: :cascade do |t|
@@ -82,7 +82,7 @@ ActiveRecord::Schema.define(version: 20180110112409) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "orders", "orders"
+  add_foreign_key "order_items", "orders"
   add_foreign_key "product_categories", "categories"
   add_foreign_key "product_categories", "products"
   add_foreign_key "product_variants", "products"
